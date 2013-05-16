d31f452a277b9180cc12952a845cca6f164a4c0b
Benjamin Jackson <bhjackson@gmail.com>
2013-05-16 16:58:40 -0400

    Fixed batch, which was ignoring options.

--- lib/biffbot/base.rb
+++ lib/biffbot/base.rb
@@ -9,7 +9,7 @@ module Biffbot
 		def parse(url,options={})
 			@url = url
 			output = Hash.new
-			request = "http://www.diffbot.com/api/article?token=#{@token}&url=#{@url}"
+			request = "http://www.diffbot.com/api/article?token=#{@token}&url=#{CGI.escape(@url)}"
 			options.each_pair do |key,value|
 				if key.match(/html|dontStripAds|tags|comments|summary|stats/) && value == true
 					request = request + "&#{key}"
@@ -25,13 +25,18 @@ module Biffbot
 			output = []
 			request = "http://www.diffbot.com/api/batch"
       batch = urls.map do |url|
-        { :method => "GET", :relative_url => "/api/article?token=#{@token}&url=#{url}" }
+        relative_url = "/api/article?token=#{@token}&url=#{CGI.escape(url)}"
+  			options.each_pair do |key,value|
+  				if key.match(/html|dontStripAds|tags|comments|summary|stats/) && value == true
+  					relative_url = relative_url + "&#{key}"
+  				end
+  			end
+        { :method => "GET", :relative_url => relative_url }
       end
       options = { :body => {:token => @token, :batch => batch.to_json }, :basic_auth => @auth }
 			response = HTTParty.post(request, options)
       
 			JSON.parse(response.parsed_response).each do |response_dict|
-        puts response_dict.inspect
 				output << JSON.parse(response_dict["body"])
 			end
 			return output
--- lib/biffbot/version.rb
+++ lib/biffbot/version.rb
@@ -1,3 +1,3 @@
 module Biffbot
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
